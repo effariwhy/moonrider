4f95a0deb2f2267e4cd95c5858766298cd64cc3e
effariwhy <effariwhy@gmail.com>
2023-11-18 15:26:04 -0500

    remove gtag

--- src/state/index.js
+++ src/state/index.js
@@ -553,14 +553,12 @@ AFRAME.registerState({
       if (state.playlist) { source = 'playlist'; }
       if (state.search.query) { source = 'search'; }
       if (state.genre) { source = 'genre'; }
-      gtag('event', 'songsource', { event_label: source });
 
       resetScore(state);
 
       // Set challenge.
       Object.assign(state.challenge, state.menuSelectedChallenge);
 
-      gtag('event', 'difficulty', { event_label: state.challenge.difficulty });
 
       // Reset menu.
       state.menuActive = false;
@@ -572,7 +570,6 @@ AFRAME.registerState({
       state.isLoading = true;
       state.loadingText = 'Loading...'
 
-      gtag('event', 'colorscheme', { event_label: state.colorScheme });
     },
 
     playlistclear: (state, playlist) => {
@@ -665,7 +662,6 @@ AFRAME.registerState({
     },
 
     songcomplete: state => {
-      gtag('event', 'songcomplete', { event_label: state.gameMode });
 
       // Move back to menu in Ride or Viewer Mode.
       if (state.gameMode === 'ride' || !state.inVR) {
@@ -737,12 +733,6 @@ AFRAME.registerState({
 
     'enter-vr': state => {
       state.inVR = AFRAME.utils.device.checkHeadsetConnected();
-      if (!AFRAME.utils.device.isMobile()) { 
-        gtag('event', 'entervr', {});
-        if (AFRAME.utils.device.isOculusBrowser()) {
-          gtag('event', 'oculusbrowser', {});
-        }
-      }
     },
 
     'exit-vr': state => {
