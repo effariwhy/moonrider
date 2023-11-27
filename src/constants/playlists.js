ce52740038d927294a5a15fd291d47493e035afc
effariwhy <effariwhy@gmail.com>
2023-11-27 14:27:17 -0500

    playlist updates

--- src/constants/playlists.js
+++ src/constants/playlists.js
@@ -1,14 +1,6 @@
 module.exports = [
-  {name: 'favorites', author: 'You!', title: 'Your Saved Favorites'},
-  {name: '3966', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 1'},
-  {name: '3967', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 2'},
-  {name: '3968', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 3'},
-  {name: '4134', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 4'},
-  {name: '4135', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 5'},
-  {name: '7483', author: 'BeastSaber', title: 'Maps Of The Weeks'},
-  {name: '4137', author: 'BeastSaber', title: "Curator's Picks of 2020"},
-  {name: '4139', author: 'BeastSaber', title: "Curator's Picks of 2021"},
-  {name: '11584', author: 'BeastSaber', title: "Curator's Picks of 2022"},
-  {name: '4405', author: 'BeastSaber', title: 'Intro to Dance'},
-  {name: '4138', author: 'BeastSaber', title: "Mom's Maps"}
+  {name: '234985', author: 'effariwhy', title: "234985 pl20231126a-80s"},
+  {name: '234988', author: 'effariwhy', title: "234988 pl20231126b-80s"},
+  {name: '9755', author: 'effariwhy', title: "9755 effariwhy"},
+  {name: '9756', author: 'effariwhy', title: "9756 try all"}
 ];
