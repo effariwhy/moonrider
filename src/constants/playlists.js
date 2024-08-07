90d71ce526614e42b3119d496d7fc93e8cf098f7
effariwhy <139010378+effariwhy@users.noreply.github.com>
2024-08-07 23:25:18 +0000

    added play dir

--- src/constants/playlists.js
+++ src/constants/playlists.js
@@ -1,7 +1,12 @@
 module.exports = [
-  {name: '242155', author: 'effariwhy', title: "effariwhy: EDM"},
-  {name: '244101', author: 'effariwhy', title: "effariwhy: Holiday"},
-  {name: '244674', author: 'effariwhy', title: "effariwhy: Pop"},
-  {name: '244673', author: 'effariwhy', title: "effariwhy: Rock and Metal"},
+  {name: '640434', author: 'effariwhy', title: "1970s"},
+  {name: '640423', author: 'effariwhy', title: "1980s"},
+  {name: '640433', author: 'effariwhy', title: "1990s"},
+  {name: '640435', author: 'effariwhy', title: "2000s"},
+  {name: '640436', author: 'effariwhy', title: "2010s"},
+  {name: '640437', author: 'effariwhy', title: "2020s"},
+  //{name: '244101', author: 'effariwhy', title: "effariwhy: Holiday"},
+  //{name: '244674', author: 'effariwhy', title: "effariwhy: Pop"},
+  //{name: '244673', author: 'effariwhy', title: "effariwhy: Rock and Metal"},
   //{name: '244674', author: 'effariwhy', title: "effariwhy: Misc"}//too big to load
 ];
