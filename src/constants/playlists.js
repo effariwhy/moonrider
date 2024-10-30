37f85510926f048f5873818ab4c7955aef807546
effariwhy <139010378+effariwhy@users.noreply.github.com>
2024-10-30 12:26:16 +0000

    playlist update

--- src/constants/playlists.js
+++ src/constants/playlists.js
@@ -1,7 +1,10 @@
 module.exports = [
   {name: 'favorites', author: 'You!', title: 'Your Saved Favorites'},
-  {name: '665016', author: 'effariwhy', title: "try"},
+  {name: '700111', author: 'effariwhy', title: "m1 - 700111"},
   {name: '654120', author: 'effariwhy', title: "approved"},
-  {name: '700111', author: 'effariwhy', title: "m1 - 700111"}
+  {name: '665016', author: 'effariwhy', title: "try"},
+  {name: '647552', author: 'effariwhy', title: "try 2"},
+  {name: '643159', author: 'effariwhy', title: "retry"},
+  {name: '665022', author: 'effariwhy', title: "X"}
   
 ];
