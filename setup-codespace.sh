03e1b972627bf9460eaffe98e5f5c6a3b43a73ea
effariwhy <139010378+effariwhy@users.noreply.github.com>
2024-12-12 14:48:52 +0000

    updated setup

--- setup-codespace.sh
+++ setup-codespace.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-
-#echo -n "$PUB_KEY" > ~/.ssh/PUB_KEY
-#chmod 644 ~/.ssh/PUB_KEY
-
-#echo -n "$PRI_KEY" > ~/.ssh/PRI_KEY
-#chmod 600 ~/.ssh/PRI_KEY
-
-echo -n "$PUB_KEY" > ~/.ssh/id_ed25519.pub
-chmod 644 ~/.ssh/id_ed25519.pub
 
 echo -n "$PRI_KEY" > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+
+if [ ! -d "/workspaces/moonrider-orig" ]; then
+    cd /workspaces
+    git clone git@github.com:supermedium/moonrider.git moonrider-orig
+    cd /workspaces/moonrider-orig
+    mv .git x.git
+fi
+
+
 
 
